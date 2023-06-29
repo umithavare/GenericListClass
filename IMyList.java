497d35c79e9e82e66f71072bb42bb6fd9c91fcd2
Umit Havare <umithavare16@gmail.com>
2023-06-29 15:53:36 +0300

    updated

---
+++ IMyList.java
@@ -0,0 +1,6 @@
+public interface IMyList {
+    int size();
+    int getCapacity();
+    void add(T value);
+    void get
+}
---
+++ MyList.java
@@ -0,0 +1,9 @@
+public class MyList <T> implements IMyList{
+    private static  int defaultCapacity = 10;
+    private Object[] array;
+    public MyList() {
+    }
+    public MyList(int capacity) {
+    }
+
+}
